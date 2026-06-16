ed1f8db56cfc169783a063f384608e9848931744
robin-hall <hallrf@merrimack.edu>
2026-06-16 18:57:46 +0000

    Deploying to gh-pages from @ robin-hall/robin-f-hall.github.io@98b849e38dfaf22934d7d73e1a2bbc6557b5d767 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "robin-f-hall/robin-f-hall.github.io",
+    "data-repo": "robin-hall/robin-f-hall.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
